793ecbbadea1b2e33d5a7780e060923fd8246d24
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-06-02 23:49:28 +0600

    delete post

--- src/pages/Post/FullPost/FullPost.jsx
+++ src/pages/Post/FullPost/FullPost.jsx
@@ -1,10 +1,14 @@
 import useFetch from "../../../hooks/useFetch.js"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import ReactMarkdown from "react-markdown"
 import { useIsOwner } from "../../../hooks/useIsOwner.js"
+import { useDispatch } from "react-redux"
+import { deletePost } from "../../../store/slices/Posts/posts.js"
 
 const FullPost = () => {
   const params = useParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const { data, loading, error } = useFetch("http://localhost:4444/posts/" + params.id)
   const isOwner = useIsOwner(data?.user?._id)
 
@@ -16,9 +20,17 @@ const FullPost = () => {
     return <p>Loading...</p>
   }
 
+  const removePostHandler = () => {
+    dispatch(deletePost(data._id))
+
+    navigate("/")
+  }
+
   return (
     <div>
       <p>Full post {isOwner ? "owner" : ""}</p>
+
+      {isOwner ? <button onClick={removePostHandler}>Remove post</button> : ""}
 
       <h1>{data.title}</h1>
 
--- src/store/slices/Posts/posts.js
+++ src/store/slices/Posts/posts.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { fetchPostBuilder } from "./fetchPost.js"
+import { PostsAPI } from "../../../API/Posts/Posts.js"
 
 const initialState = {
   posts: {
@@ -11,10 +12,17 @@ const initialState = {
 export const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    deletePost(state, action) {
+      state.posts.data = state.posts.data.filter(post => post._id !== action.payload)
+      PostsAPI.deletePost(action.payload)
+    },
+  },
   extraReducers: (builder) => {
     fetchPostBuilder(builder)
   },
 })
+
+export const { deletePost } = postsSlice.actions
 
 export const postsReducer = postsSlice.reducer
