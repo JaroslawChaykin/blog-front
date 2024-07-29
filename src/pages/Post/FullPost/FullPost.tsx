e95b2b5c69528bbf98fa040e59727c8b6bc94406
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-07-29 11:22:26 +0600

    fullPost styles

--- src/pages/Post/FullPost/FullPost.tsx
+++ src/pages/Post/FullPost/FullPost.tsx
@@ -6,9 +6,12 @@ import { deletePost } from "../../../store/slices/Posts/posts"
 import { IPost } from "../../../API/Posts/Posts"
 import { useAppDispatch } from "../../../hooks/useAppDispatch"
 import { BASE_URL } from "../../../constants"
-import { Button, Title } from "../../../UI"
+import { Button, Tag, Text, Title } from "../../../UI"
 import { FaTrash } from "react-icons/fa"
 import { RiEdit2Fill } from "react-icons/ri"
+import { IoMdEye } from "react-icons/io"
+import Collection from "../../../components/Collection/Collection"
+import { formattingDate } from "../../../utils"
 import cl from "./FullPost.module.scss"
 
 const FullPost = () => {
@@ -17,6 +20,8 @@ const FullPost = () => {
   const dispatch = useAppDispatch()
   const { data, loading, error } = useFetch<IPost>(`${BASE_URL}/posts/${params.id}`)
   const isOwner = useIsOwner(data?.user?._id || "")
+  const { year, day, monthName, hours, minutes } = formattingDate(data?.createdAt)
+  const formattedDate = `${monthName} ${day}, ${year} ${hours}:${minutes}`
 
   if (error) {
     return <p>Error</p>
@@ -34,24 +39,53 @@ const FullPost = () => {
 
   return (
     <div className={cl.fullPost}>
-      {isOwner ? (
-        <div className={cl.controlBtns}>
-          <Button onClick={removePostHandler} variant="danger" size="md">
-            <FaTrash />
-          </Button>
-          <Button onClick={() => navigate(`/posts/${data._id}/edit`)} variant="primary" size="md">
-            <RiEdit2Fill />
-          </Button>
+      <div className={cl.postImage}>
+        {data.imageUrl ? (
+          <img src={`http://localhost:4444/${data.imageUrl}`} alt={data.imageUrl} />
+        ) : (
+          <div className={cl.imagePlaceholder}></div>
+        )}
+
+        <div className={cl.postInfoBox}>
+          <div className={cl.postInfoContent}>
+            <Text>{formattedDate}</Text>
+            <Button size="md" onClick={() => navigate(`/${data.user.nickname}`)}>
+              {data.user.nickname}
+            </Button>
+            <Text>
+              <IoMdEye /> {data.viewsCount}
+            </Text>
+          </div>
         </div>
-      ) : (
-        ""
-      )}
 
-      <Title size="4xl" how="h1">
-        {data.title}
-      </Title>
-
+        {isOwner ? (
+          <div className={cl.controlBtns}>
+            <Button onClick={removePostHandler} variant="danger" size="md">
+              <FaTrash />
+            </Button>
+            <Button onClick={() => navigate(`/posts/${data._id}/edit`)} variant="primary" size="md">
+              <RiEdit2Fill />
+            </Button>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
       <div className={cl.content}>
+        <Title size="4xl" how="h1">
+          {data.title}
+        </Title>
+
+        <Collection
+          orientation="horizontal"
+          listOfData={data.tags.slice(0, 2)}
+          displayData={(item, index) => (
+            <Tag variant="solid" size="sm" key={index}>
+              {item}
+            </Tag>
+          )}
+        />
+
         <ReactMarkdown children={data.text} />
       </div>
     </div>
