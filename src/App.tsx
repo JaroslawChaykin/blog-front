d29e463176295210b601aca737f2ba54731507a0
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-07-30 21:31:17 +0600

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import AppRouter from "./router/AppRouter"
 import { Provider } from "react-redux"
 import store from "./store/store"
+import "./assets/fonts/Gilroy-Regular.woff"
+import "./assets/fonts/Gilroy-Bold.woff"
 import "./App.scss"
 
 function App() {
