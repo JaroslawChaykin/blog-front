1c22f91b2bedeb02fa541545143b88154c8d8e6f
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-08-14 23:54:55 +0600

    new profilePosts page

--- src/pages/Profile/ProfilePosts/ProfilePosts.tsx
+++ src/pages/Profile/ProfilePosts/ProfilePosts.tsx
@@ -1,7 +1,29 @@
-import React from "react"
+import { useEffect, useState } from "react"
+import { IPost, PostsAPI } from "../../../API/Posts/Posts"
+import PostController from "../../../components/PostController/PostController"
+import Collection from "../../../components/Collection/Collection"
 
 const ProfilePosts = () => {
-  return <div>Profile posts</div>
+  const [posts, setPosts] = useState<IPost[]>()
+
+  useEffect(() => {
+    const getOwnerPosts = async () => {
+      const actualPosts = await PostsAPI.getOwnerPosts()
+
+      setPosts(actualPosts)
+    }
+
+    getOwnerPosts()
+  }, [])
+
+  if (!posts) {
+    return <span>Not found posts</span>
+  }
+  return (
+    <div>
+      <Collection listOfData={posts} displayData={(post) => <PostController post={post} />} />
+    </div>
+  )
 }
 
 export default ProfilePosts
